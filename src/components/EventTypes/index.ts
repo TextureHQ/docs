27546f0c0a583a532f01e25274de842f8f9cd265
Will Laeri <wlaeri@gmail.com>
2025-02-09 15:59:59 -0500

    device updated event

--- src/components/EventTypes/index.ts
+++ src/components/EventTypes/index.ts
@@ -1,5 +1,6 @@
 export * from "./CustomerCreated";
 export * from "./CommandFailed";
 export * from "./CommandSucceeded";
+export * from "./DeviceUpdated";
 export * from "./EnrollmentApproved";
 export * from "./SiteCreated";
